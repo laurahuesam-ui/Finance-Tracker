5110cf56c774d04a1f5c10e2f432e687a14dc1ce
laurahuesam-ui <laura.huesam@gmail.com>
2026-08-05 12:35:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
-
-const CACHE="finanzen-pwa-v41";
+const CACHE="finanzen-pwa-v48";
 const FILES=["./","index.html","style.css","app.js","manifest.webmanifest","icon-192.png","icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener("fetch",e=>{
+  const req=e.request;
+  if(req.method!=="GET")return;
+  e.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res;}).catch(()=>caches.match(req)));
+});
